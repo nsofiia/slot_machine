1adc9daa99e4a70918794e9b5663ccbc0fd63675
Sofi <nsofiia01@gmail.com>
2022-12-14 19:42:24 -0600

    latest, figuring out methods with array

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace slot_machine;
+﻿using System.Security.Cryptography;
+using static slot_machine.UI;
+
+namespace slot_machine;
 class Program
 {
     const double BET_MIN = 2.0;    //bets and wins initialized as constants
@@ -6,84 +9,56 @@ class Program
     const double BET_MIN_WIN = 4.0;
     const double BET_MAX_WIN = 6.0;
 
-    public static void firstCheck()
-    {
-        Console.WriteLine("First combination");
-    }
-
-    public static void secondCheck()
-    {
-        Console.WriteLine("Second combination");
-    }
-
-    public static void thirdCheck()
-    {
-        Console.WriteLine("Third combination");
-    }
-
-    public static void win()
-    {
-        System.Threading.Thread.Sleep(500); //all match in 1 combination
-        Console.WriteLine("WIN");
-    }
-
-    public static void notWin()  //combination has no match
-    {
-        System.Threading.Thread.Sleep(500);
-        Console.WriteLine("nope\n");
-    }
-
     static void Main(string[] args)
     {
-        Random randomNum = new Random();  //random 
-        int[,] slotMachine = new int[3, 3]; //creating empty 2D array with 3 rows and 3 columns
-        char[] playChoices = new char[4] { 'V', 'H', 'D', 'M' }; // creating betting choices for player to bet on; having "const" in front of the above line is thrwing error, moved to the bottom to have it accesible by the next loop
-        char lineChoise; //line choice have no value, expected to have input
-        List<string> compliment = new List<string> { "Great choise!", "Awesome selection!", "Let's get to it!", "GOOD choise!", "You are a player!" }; //list of encouragements added 
-        double balance = 20.0; //initial ballance of the player
-        char playAgain; //restart option
+        Random randomNum = new Random();
+        //random
+        int[,] slotMachine = new int[3, 3];
+        //creating empty 2D array with 3 rows and 3 columns
+        char[] playChoices = new char[4] { 'V', 'H', 'D', 'M' };
+        // creating betting choices for player to bet on; having "const" in front of the above line is thrwing error, moved to the bottom to have it accesible by the next loop
+        char lineChoise;
+        //line choice have no value, expected to have input
+        List<string> compliment = new List<string> { "Great choise!", "Awesome selection!", "Let's get to it!", "GOOD choise!", "You are a player!" };
+        //list of encouragements for the user 
+        double balance = 20.0;
+        //initial ballance of the player
+        char playAgain;
+        //restart option
+        
 
-
-
-        while (true) //start of the game
+        while (true)
+        //start of the game
         {
-            Console.WriteLine($"Slot machine has infinite ways for you to win ;)\nWINNING gets you a DOUBLE of the bet!\n" +
-            $"EACH additional winning line is geting you even MORE! (min2-max3 lines per 1 bet)\n");
-
-            while (true) //inside of the game after each bet
+            printGreetingAndRules();
+            
+            while (true)
+            //inside of the game after each bet
             {
                 string complimentAtTime = compliment[randomNum.Next(compliment.Count)];
+                //select random number for a compliment list string
                 int winCount = 0;
-                Console.WriteLine($"Current balance {balance.ToString("C")}\n");
-                Console.WriteLine($"Betting categories:\n" +
-                    $"{playChoices[0]} - 3 VERTICAL lines for {BET_MAX.ToString("C")}\n" +      //bet min
-                    $"{playChoices[1]} - 3 HORIZONTALlines for {BET_MAX.ToString("C")}\n" +    //bet min
-                    $"{playChoices[2]} - 2 DIAGONAL lines for {BET_MIN.ToString("C")}\n" +    //bet max
-                    $"{playChoices[3]} - 2 MIDDLE lines for {BET_MIN.ToString("C")}\n" +     //bet max
-                    $"\nenter your betting choice V, H, D or M\n");
+                //for gain calculation
+                balanceDisplay(balance);
+                bettingChoicesDisplay(playChoices[0], playChoices[1], playChoices[2], playChoices[3], BET_MIN, BET_MAX);
 
-
-                lineChoise = Char.ToUpper(Console.ReadKey().KeyChar);
-
-                while (!playChoices.Contains(lineChoise)) //checks if input is anything but a letter, if not, repeats
-                {
-                    Console.WriteLine("\nChoose one of the letters above.\n");
-                    lineChoise = Char.ToUpper(Console.ReadKey().KeyChar);
-                }
+                lineChoise = //getValueUntilIsCorrect(lineChoise, playChoices, lineChoise);
 
                 Console.Clear();
-                Console.WriteLine(complimentAtTime + "\n"); //random encouragement for the user
-                Console.WriteLine("Fetching the numbers"); //hint to wait
-
+                complimentPlusWaitPrompt(complimentAtTime);
 
                 for (int row = 0; row < slotMachine.GetLength(0); row++) //printing 3x3 matrix with random number in each cell
                 {
                     for (int column = 0; column < slotMachine.GetLength(1); column++)
                     {
-                        slotMachine[row, column] = randomNum.Next(3);    //only including numbers from 0 to 2 
-                        Console.Write(slotMachine[row, column] + " "); //print each cell
-                        System.Threading.Thread.Sleep(500);  //wait before printing next one
-                        if (column == slotMachine.GetUpperBound(0)) // add new line when finished with a row -- when last column index == to last index in the 
+                        slotMachine[row, column] = randomNum.Next(3);
+                        //only including numbers from 0 to 2 
+                        Console.Write(slotMachine[row, column] + " ");
+                        //print each cell
+                        System.Threading.Thread.Sleep(500);
+                        //wait before printing next one
+                        if (column == slotMachine.GetUpperBound(0))
+                        //add new line when finished with a row -- when last column index == to last index in the first dimension add new line
                         {
                             Console.WriteLine();
                         }
@@ -94,7 +69,7 @@ class Program
                 {
 
                     balance -= BET_MIN; //removing betting cost
-                    Console.WriteLine($"Balance {balance.ToString("C")}\n");
+                    balanceDisplay(balance);
 
                     firstCheck();
                     if (slotMachine[0, 0] == slotMachine[1, 1] && slotMachine[1, 1] == slotMachine[2, 2]) //are right top and middle cells equal?
@@ -126,7 +101,7 @@ class Program
                 if (lineChoise == 'M')
                 {
                     balance -= BET_MIN;
-                    Console.WriteLine($"\nBalance {balance.ToString("C")}\n");
+                    balanceDisplay(balance);
 
                     firstCheck();
                     if (slotMachine[0, 1] == slotMachine[1, 1] && slotMachine[1, 1] == slotMachine[2, 1])
@@ -158,7 +133,7 @@ class Program
                 if (lineChoise == 'V')
                 {
                     balance -= BET_MAX;
-                    Console.WriteLine($"Balance {balance.ToString("C")}\n");
+                    balanceDisplay(balance);
 
                     firstCheck();
                     if (slotMachine[0, 0] == slotMachine[1, 0] && slotMachine[1, 0] == slotMachine[2, 0])
@@ -203,7 +178,7 @@ class Program
                 if (lineChoise == 'H')
                 {
                     balance -= BET_MAX;
-                    Console.WriteLine($"Balance {balance.ToString("C")}\n");
+                    balanceDisplay(balance);
 
                     firstCheck();
                     if (slotMachine[0, 0] == slotMachine[0, 1] && slotMachine[0, 1] == slotMachine[0, 2])
@@ -264,7 +239,7 @@ class Program
                     Console.WriteLine("no winning combinations");
                 }
 
-                Console.WriteLine($"New balance {balance.ToString("C")}");
+                balanceDisplay(balance);
                 Console.WriteLine("\nTry again?:\ny - to play, \nany other key to exit \n");
 
                 playAgain = Char.ToUpper(Console.ReadKey().KeyChar);
---
+++ UI.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace slot_machine
+{
+    public static class UI
+    {
+        /// <summary>
+        /// is listing greeting and rules
+        /// </summary>
+        public static void printGreetingAndRules()
+        {
+            Console.WriteLine($"Slot machine has infinite ways for you to win ;)\nWINNING gets you a DOUBLE of the bet!\n" +
+            $"EACH additional winning line is geting you even MORE! (min2-max3 lines per 1 bet)\n");
+        }
+
+
+        /// <summary>
+        /// is displaying current balance
+        /// </summary>
+        /// <param name="balance"></param>
+        public static void balanceDisplay(double balance)
+        {
+            Console.WriteLine($"\nBalance {balance.ToString("C")}\n");
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="option1"></param>first
+        /// <param name="option2"></param>second
+        /// <param name="option3"></param>third
+        /// <param name="option4"></param>forth
+        /// <param name="moneyMin"></param>small bet
+        /// <param name="moneyMax"></param>biger bet
+        public static void bettingChoicesDisplay(char option1, char option2, char option3, char option4, double moneyMin, double moneyMax)
+        {
+            Console.WriteLine($"Betting categories:\n" +
+                    $"{option1} - 3 VERTICAL lines for {moneyMax.ToString("C")}\n" +      //bet max
+                    $"{option2} - 3 HORIZONTALlines for {moneyMax.ToString("C")}\n" +    //bet max
+                    $"{option3} - 2 DIAGONAL lines for {moneyMin.ToString("C")}\n" +    //bet min
+                    $"{option4} - 2 MIDDLE lines for {moneyMin.ToString("C")}\n" +     //bet min
+                    $"\nenter your betting choice V, H, D or M\n");
+        }
+
+
+
+
+        //public static char getValueUntilIsCorrect(char input, List<char> choices, out char output)
+        //{
+        //    while (!choices.Contains(input)) //checks if input is anything but a letter, if not, repeats
+        //    {
+        //        Console.WriteLine("\nChoose one of the letters above.\n");
+        //        input = Char.ToUpper(Console.ReadKey().KeyChar);
+        //        return output = input;
+        //    }
+        //}
+
+        /// <summary>
+        /// taking in random compliment, prints it and additional text
+        /// </summary>
+        /// <param name="randomCompliment"></param>
+        public static void complimentPlusWaitPrompt(string randomCompliment)
+        {
+            Console.WriteLine(randomCompliment + "\n"); //random encouragement for the user
+            Console.WriteLine("Fetching the numbers"); //hint to wait
+
+
+
+        }
+
+        public static void printMatrix(int slotMachine[row, column])
+        {
+            Console.Write(slotMachine[row, column] + " ");
+        }
+
+
+
+
+
+
+
+        public static void firstCheck()
+        {
+            Console.WriteLine("First combination");
+        }
+
+        public static void secondCheck()
+        {
+            Console.WriteLine("Second combination");
+        }
+
+        public static void thirdCheck()
+        {
+            Console.WriteLine("Third combination");
+        }
+
+        public static void win()
+        {
+            System.Threading.Thread.Sleep(500); //all match in 1 combination
+            Console.WriteLine("WIN");
+        }
+
+        public static void notWin()  //combination has no match
+        {
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("nope\n");
+        }
+
+    }
+}
+
